460d85c48ce0cf59237b85aed57e3ab6ce57f095
ryuu523 <ryu20050523@yahoo.co.jp>
2024-10-30 16:00:06 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import { Navigate, Route, BrowserRouter as Router, Routes, } from 'react-router-dom';
-import './App.css';
-import Start from './pages/Start';
-import AuthLayout from './components/AuthLayout';
-import Game from './pages/Game';
-import Select from './pages/Select';
-import Profile from './pages/Profile';
-import Clear from './pages/Clear';
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Start />}></Route>
-        <Route path='/' element={<AuthLayout />}>
-          <Route path="game" element={<Game />} />
-          <Route path="select" element={<Select />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="clear" element={<Clear />} />
-
-        </Route>
-        <Route path="*" element={<Navigate to="/"/>}/>
-      </Routes>
-    </Router>
+  <>
+  </>
   );
 }
 
--- src/components/formatTime.js
+++
@@ -1,8 +0,0 @@
-export const formatTimeMS = (time) => {
-    const m = Math.floor(time / 60)
-    let s = time % 60
-    if (s < 10) {
-        s = "0" + s
-    }
-    return `${m}:${s}`
-}
